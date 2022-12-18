a43785751b24898cd0d06bf85227d1fd42036596
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2022-12-18 01:35:11 -0300

    1010100101010101010101

--- public/Desc.js
+++ public/Desc.js
@@ -2,8 +2,8 @@ const Enlaces = [
   "https://react-cms-omega.vercel.app/?txt=0",
   "https://react-cms-omega.vercel.app/?txt=1",
   "https://react-cms-omega.vercel.app/?txt=2",
-  "https://react-cms-omega.vercel.app/?txt=8",
-  "https://react-cms-omega.vercel.app/?txt=9",
-  "https://react-cms-omega.vercel.app/?txt=10"
+  "https://react-cms-omega.vercel.app/?txt=3",
+  "https://react-cms-omega.vercel.app/?txt=4",
+  "https://react-cms-omega.vercel.app/?txt=5"
 ];
 const x = 0;
