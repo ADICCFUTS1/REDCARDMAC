07815bbf8bdb394dca00937cbb3a291854dbea5b
ADICCFUTS1 <95947821+ADICCFUTS1@users.noreply.github.com>
2023-01-10 16:32:35 -0300

    0110101010

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  // ...
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      })
+    ]
+  }
+  // ...
+};
